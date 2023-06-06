1e83973167513f13d5331f93eeb933678fb5a203
lzx0322 <127064636+lzx0322@users.noreply.github.com>
2023-06-06 20:55:46 +0800

    项目起步完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,9 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
 
 export default createStore({
   state: {
@@ -10,5 +15,16 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
+    cart,
+    user,
+    category
+  },
+  // 配置插件
+  plugins: [createPersistedState({
+    // 默认存储在 localStorage
+    // 本地存储名字
+    key: 'erabbit-client-pc-store',
+    // 指定需要存储的模块
+    paths: ['user', 'cart']
+  })]
 })
