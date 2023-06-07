b5ef5c99256f6e331fce4e69e2ec37b1e7eb41c5
lzx0322 <127064636+lzx0322@users.noreply.github.com>
2023-06-07 15:19:01 +0800

    首页模块第二次提交

--- src/views/member/layout.vue
+++ src/views/member/layout.vue
@@ -1,20 +1,28 @@
 <template>
   <!-- 顶部通栏 -->
-  <nav>顶部通栏</nav>
+  <app-navbar></app-navbar>
   <!-- 头部组件-->
-  <header>头部组件</header>
+  <app-header></app-header>
   <!-- 内容容器 -->
   <div class="main">
     <!-- 二级路由 -->
     <router-view></router-view>
   </div>
   <!-- 底部组件 -->
-  <footer>底部组件</footer>
+  <app-footer></app-footer>
 </template>
 
 <script>
+import AppNavbar from '@/components/app-topnav.vue'
+import AppHeader from '@/components/app-header.vue'
+import AppFooter from '@/components/app-footer.vue'
 export default {
-  name: 'Layout'
+  name: 'Layout',
+  components: {
+    AppNavbar,
+    AppHeader,
+    AppFooter
+  }
 }
 </script>
 
